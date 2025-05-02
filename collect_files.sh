c297d55667e5415c25f5b56553bdfcec71666f5b
artemgushchin01 <avgushchin@edu.hse.ru>
2025-05-02 13:19:33 +0400

    tes4

--- collect_files.sh
+++ collect_files.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 find "$1" -type f -exec cp {} "$2" \;
+
+#https://habr.com/ru/articles/726316/
+#https://gist.github.com/Titiaiev/dcb7298389d1276b823bbc96e29f940d
+#https://proglib.io/p/bash-commands-for-beginners
+#https://raybin.ru/osnovnye-komandy-bush.html
+#https://ugolok.vercel.app/books/bash.pdf
